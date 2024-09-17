ad19cc055b9394de402b78117eec847282359f00
Mezbaur Are Rafi <133680494+mezbaur2004@users.noreply.github.com>
2024-09-18 02:33:39 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./components/homePage.jsx";
-import ContactPage from './components/ContactPage';
+import ContactPage from './components/contactPage';
 import AllProjects from "./components/allProjects.jsx";
 import LoginPage from "./components/loginPage.jsx";
 import AdminPage from "./components/adminPage.jsx";
