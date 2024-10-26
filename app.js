49a56b3ab41ccd9f090f8975c8f9180b823f3231
Mezbaur Are Rafi <mezbaur2004@gmail.com>
2024-10-27 00:10:50 +0600

    csp updated

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
         "default-src 'self'; " +
         "script-src 'self' https://cdn.jsdelivr.net https://kit.fontawesome.com; " +
         "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com https://ka-f.fontawesome.com; " +
-        "img-src 'self' https://simpauldesign.com https://*.facebook.com data:; " + // Added Facebook image domain
+        "img-src 'self' https://simpauldesign.com https://*.facebook.com https://i.ibb.co data:; " + // Added image domain
         "font-src 'self' https://fonts.gstatic.com https://ka-f.fontawesome.com; " +
         "connect-src 'self' https://ka-f.fontawesome.com;"
     );
